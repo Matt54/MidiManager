6adf275c412a609914f6a149f60a50037be96056
Matt Pfeiffer <Mattpfeiffer54@gmail.com>
2022-03-23 17:08:41 -0500

    Changed midi port accessors
    Changed midi port accessors

--- Sources/MidiManager/MidiPort.swift
+++ Sources/MidiManager/MidiPort.swift
@@ -2,10 +2,10 @@ import CoreMIDI
 
 public struct MidiPort: Identifiable {
     public var id: MIDIUniqueID { midiUniqueID }
-    let midiUniqueID: MIDIUniqueID
-    let name: String
-    var isConnected: Bool = false
-    var isInput: Bool = true
+    public let midiUniqueID: MIDIUniqueID
+    public let name: String
+    public var isConnected: Bool = false
+    public var isInput: Bool = true
     
     public init(midiUniqueID: MIDIUniqueID, name: String, isConnected: Bool = false, isInput: Bool = true) {
         self.midiUniqueID = midiUniqueID
