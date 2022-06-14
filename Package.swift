130bcc447f2316be8f965f4e7948aed9b77ea77f
Matt Pfeiffer <Mattpfeiffer54@gmail.com>
2022-06-14 16:43:43 -0500

    Updated AudioKit branch to main
    Updated AudioKit branch to main

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/AudioKit/AudioKit", branch:"develop"),
+        .package(url: "https://github.com/AudioKit/AudioKit", branch:"main"),
         .package(url: "https://github.com/Matt54/UtilityAndBeyond.git", branch:"main")
     ],
     targets: [
