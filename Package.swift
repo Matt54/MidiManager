8c38a8c5e0897f2864b3f9b7f587c51220520ec6
Matt Pfeiffer <Mattpfeiffer54@gmail.com>
2022-11-13 11:26:39 -0600

    Lowered minimum macOS version
    Lowered minimum macOS version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MidiManager",
     platforms: [
-        .macOS("12.0"), .iOS(.v14), .tvOS(.v13)
+        .macOS("11.0"), .iOS(.v14), .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
