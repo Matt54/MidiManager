a40c024d5a96d911dc2fb26695beb9a1bccbd6cd
Matt Pfeiffer <Mattpfeiffer54@gmail.com>
2023-03-28 06:54:27 -0500

    Added getMockPorts and WIP VirtualMidiPort

--- Sources/MidiManager/MidiPort.swift
+++ Sources/MidiManager/MidiPort.swift
@@ -18,4 +18,26 @@ public struct MidiPort: Identifiable {
         let randomId = Int32.random(in: 1000000000...1999999999)
         return MidiPort(midiUniqueID: -randomId, name: "Mock Port Name", isConnected: isConnected, isInput: isInput)
     }
+    
+    public static func getMockPorts(isInput: Bool = false) -> [MidiPort] {
+        var ports = [MidiPort]()
+        for _ in 0...3 {
+            let port = MidiPort.getMockPort(isConnected: Bool.random(), isInput: isInput)
+            ports.append(port)
+        }
+        return ports
+    }
+}
+
+/// WIP - don't use yet
+public struct VirtualMidiPort: Identifiable {
+    public var id: MIDIPortRef
+    public let name: String
+    public var isConnected: Bool = false
+    
+    public init(id: MIDIPortRef, name: String, isConnected: Bool = false) {
+        self.id = id
+        self.name = name
+        self.isConnected = isConnected
+    }
 }
